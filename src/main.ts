097b104682edfda170e6d60bd34f20a857be0e17
MaxStankevich <makstan1319@gmail.com>
2024-06-26 01:17:41 +0200

    Update configuration for Vercel deployment

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
   const configService = app.get(ConfigService);
-
   const allowedOrigins = configService.get('ALLOWED_ORIGINS')?.split(',') || [];
 
   const corsOptions: CorsOptions = {
@@ -23,6 +26,13 @@ async function bootstrap() {
   };
 
   app.enableCors(corsOptions);
-  await app.listen(3001);
+
+  if (process.env.VERCEL_ENV) {
+    await app.init();
+  } else {
+    await app.listen(3001);
+    console.log('Application is running on: http://localhost:3001');
+  }
 }
+
 bootstrap();
